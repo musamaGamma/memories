0e564a7baa2de6dbee57d6406520964b734e4d79
musamaGamma <johan0373@gmail.com>
2020-12-18 20:39:58 +0300

    main page

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-
+const path = require("path")
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
@@ -27,6 +27,15 @@ app.use("/posts", require("./routes/posts"))
 app.use("/", (req, res)=> {
     res.send("welcome to memories api")
 })
-
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/client/build")))
+  
+    app.get("*", (req, res)=> res.sendFile(path.resolve(__dirname, "client", "build", "index.html")))
+  }
+  else {
+    app.get("/", (req, res) => {
+      res.send("api is running");
+    });
+  }
 const port = process.env.PORT || 5000
 app.listen(port, ()=> console.log(`listening on port ${port}`))
